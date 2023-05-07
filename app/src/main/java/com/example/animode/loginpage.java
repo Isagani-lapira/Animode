7a11821c5890a478b576f59189407e81f3fe8081
Isagani-lapira <lapiraisagani@gmail.com>
2023-05-07 10:13:34 +0800

    Loginpage

---
+++ app/src/main/java/com/example/animode/loginpage.java
@@ -0,0 +1,28 @@
+package com.example.animode;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+
+
+public class loginpage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.login_activity);
+
+        initialize();
+    }
+
+    private void initialize() {
+
+
+    }
+}
