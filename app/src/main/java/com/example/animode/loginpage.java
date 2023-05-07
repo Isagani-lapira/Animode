b2a5d8b02fcbea9f39bde03dc3665395b457998f
Isagani-lapira <lapiraisagani@gmail.com>
2023-05-07 18:08:12 +0800

    firebase

--- app/src/main/java/com/example/animode/loginpage.java
+++ app/src/main/java/com/example/animode/loginpage.java
@@ -14,6 +14,8 @@ public class loginpage extends AppCompatActivity {
     private EditText etEmail, etPass;
     private TextView tvRegister;
     private Button signin;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +24,6 @@ public class loginpage extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.login_activity);
-
         initialize();
     }
 
